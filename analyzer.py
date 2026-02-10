84cc34e56aa55ec6d386dd526aef6d332266f54b
Asher Pirov <Asherpirov@gmail.com>
2026-02-10 14:19:31 +0200

    Add get_sensitive_port_traffic function

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,8 @@
 def get_external_ips(data):
     external_ips = [row[1] for row in data if not row[1].startswith(("192.168.", "10."))]
     return external_ips
+
+def get_sensitive_port_traffic(data):
+    sensitive_ports = ["22","23", "3389"]
+    sensitive_traffic = [row for row in data if row[3] in sensitive_ports]
+    return sensitive_traffic
